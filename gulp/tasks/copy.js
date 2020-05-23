d105c2a9ef66be13999f3bac14c7a821ee72ef80
makis <mazirat@gmail.com>
2020-05-23 14:35:55 +0300

    add external js

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,6 +2,7 @@ module.exports = function () {
   $.gulp.task('copy', function() {
     return $.gulp.src([
       'fonts/**/*.{woff,woff2}',
+      'js/external/*.js',
     ], {
       base: '.'
     })
