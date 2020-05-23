27e81625d21fb7c843fa41b2980398869c184230
makis <mazirat@gmail.com>
2020-05-23 14:50:24 +0300

    fix js external path

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,7 +2,7 @@ module.exports = function () {
   $.gulp.task('copy', function() {
     return $.gulp.src([
       'fonts/**/*.{woff,woff2}',
-      'js/external/*.js',
+      'js/external/**/*.js',
       'js/external/**/*.css',
     ], {
       base: '.'
