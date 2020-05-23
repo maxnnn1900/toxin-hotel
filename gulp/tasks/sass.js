cd63f1ada9b5c278d12e87073e906d02d7fb3036
makis <mazirat@gmail.com>
2020-05-23 22:51:33 +0300

    add dev and prod mode to sass task

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,10 +4,14 @@ module.exports = function () {
       .pipe($.glp.plumber())
       .pipe($.glp.sourcemaps.init())
       .pipe($.glp.sass())
-      .pipe($.glp.autoprefixer({
-        overrideBrowserslist: ['last 2 versions']
-      }))
-      .pipe($.glp.csso())
+      .pipe(
+        ($.glp.if(process.env.NODE_ENV == 'production', 
+          $.glp.autoprefixer({
+            overrideBrowserslist: ['last 2 versions']
+          }),
+          $.glp.csso()
+        ))        
+      )
       .pipe($.glp.rename('style.min.css'))
       .pipe($.glp.sourcemaps.write(''))
       .pipe($.gulp.dest('build/css'))
