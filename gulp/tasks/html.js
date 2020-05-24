24380d619a5f91d118743d6267283593fc03d66e
makis <mazirat@gmail.com>
2020-05-24 19:14:53 +0300

    add list of pages

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,7 @@
 module.exports = function () {
   $.gulp.task('html', function() {
     return $.gulp.src('*.html')
+      .pipe($.glp.addSrc('html/**/*.html'))
       .pipe($.glp.plumber())
       .pipe($.glp.include())
       .pipe($.gulp.dest('build'))
